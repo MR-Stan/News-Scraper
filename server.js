c4542d40af4c302704f7404f1704e6035beb36c2
MR-Stan <michaelrstanley87@gmail.com>
2020-01-19 17:16:45 -0600

    initial file creation, installed dependencies

---
+++ server.js
@@ -0,0 +1,8 @@
+// dependencies
+const express = require('express');
+const axios = require('axios');
+const mongo = require('mongojs');
+const cheerio = require('cheerio');
+
+// initialize express
+const app = express();
