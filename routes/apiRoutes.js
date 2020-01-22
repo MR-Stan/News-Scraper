d462c65b205012b6746284cb98905d3f17b626d7
MR-Stan <michaelrstanley87@gmail.com>
2020-01-21 18:20:58 -0600

    updated from scraping twitter to npr

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,26 +5,30 @@ const cheerio = require('cheerio');
 const db = require('../db');
 
 module.exports = function (app) {
-
-    axios.get('https://twitter.com/technews_today?lang=en')
+    // need actual articles
+    axios.get('https://www.npr.org/sections/technology/')
         .then(function (response) {
             //console.log(response.data);
             const $ = cheerio.load(response.data);
             let results = [];
-            $('.ProfileTweet').each(function (i, element) {
+            $('article').each(function (i, element) {
                 results.push({
-                    user: $(element).attr('data-screen-name'),
-                    tweet_id: $(element).attr('data-tweet-id'),
-                    tweet: $(element).find('.js-tweet-text').text()
+                    // headline
+                    // summary
+                    // url
+                    // additional information
+                    // user: $(element).attr('data-screen-name'),
+                    // tweet_id: $(element).attr('data-tweet-id'),
+                    // tweet: $(element).find('.js-tweet-text').text()
                 });
             });
-            db.scraper.insert(results);
+            // db.scraper.insert(results);
         })
         .catch(function (error) {
             console.log(error);
         })
 
-    app.get("/all", function (req, res) {
+    app.get("/", function (req, res) {
         db.scraper.find({}, function (error, found) {
             if (error) {
                 console.log(error);
