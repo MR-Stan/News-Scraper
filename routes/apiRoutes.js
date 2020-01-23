99ebcd009613472e01ebd5256623c70697643628
MR-Stan <michaelrstanley87@gmail.com>
2020-01-23 15:39:20 -0600

    scraped article links working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,40 +5,50 @@ const cheerio = require('cheerio');
 const db = require('../models');
 
 module.exports = function (app) {
-    app.get('scrape', function (req, res) {
+
+    // Scrape web and add articles to db
+    app.get('/scrape', function (req, res) {
         axios.get('https://www.npr.org/sections/technology/')
             .then(function (response) {
                 const $ = cheerio.load(response.data);
-
-                let results = [];
                 $('article.item').each(function (i, element) {
-                    // results.push({
-                    //     headline
-                    //     summary
-                    //     url
-                    //     additional information
-                    //     user: $(element).attr('data-screen-name'),
-                    //     tweet_id: $(element).attr('data-tweet-id'),
-                    //     tweet: $(element).find('.js-tweet-text').text()
-                    // });
-                    console.log(element);
-                });
+                    let result = {};
+                    // result.headline = $(this)
+                    //     .children('whatever element')
+                    //     .text();
+                    // result.summary = $(this)
+                    //     .children()
+                    //     .text()
+                    result.link = $(this)
+                        .children('div.item-image')
+                        .children('div.imagewrap')
+                        .children('a')
+                        .attr('href')
 
+                    // db.Article.create(result)
+                    //     .then(function (dbArticle) {
+                    //         console.log(dbArticle);
+                    //     })
+                    //     .catch(function (err) {
+                    //         console.log(err);
+                    //     });
+                });
             })
             .catch(function (error) {
                 console.log(error);
             })
+        res.send('Scrape complete');
     });
 
-    app.get("/", function (req, res) {
-        db.scraper.find({}, function (error, found) {
-            if (error) {
-                console.log(error);
-            }
-            else {
-                res.json(found);
-            }
-        });
+    // Get all articles from db
+    app.get("/articles", function (req, res) {
+        db.Article.find({})
+            .then(function (dbArticle) {
+                res.json(dbArticle);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
     });
 
 
